53b4c00f4d91d9f4f5c3feaff4d90eae32377bd2
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-03-31 21:58:31 +0200

    feat: class Genre created

--- index.php
+++ index.php
@@ -1,16 +1,66 @@
 <?php
 // definizione di una classe Movie
+class Genre
+{
+
+    protected $name;
+    protected $description;
+    //protected $ageRating;
+
+    public function __construct($_name, $_description) //, $_ageRating)
+    {
+        $this->name = $_name;
+        $this->description = $_description;
+        //$this->ageRating = $_ageRating;
+    }
+
+    public function setName($_name)
+    {
+        $this->name = $_name;
+    }
+
+    public function setDescription($_description)
+    {
+        $this->description = $_description;
+    }
+
+    // public function setAgeRating($_ageRating)
+    // {
+    //     $this->ageRating = $_ageRating;
+    // }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    // public function getAgeRating()
+    // {
+    //     return $this->ageRating;
+    // }
+
+}
+
+
 class Movie
 {
     protected $title;
     protected $description;
     protected $year;
 
-    public function __construct($_title, $_description, $_year)
+    protected $genre;
+
+    public function __construct($_title, $_description, $_year, Genre $_genre)
     {
         $this->title = $_title;
         $this->description = $_description;
         $this->year = $_year;
+        $this->genre = $_genre;
 
     }
 
@@ -48,14 +98,14 @@ $film1_title = "Titanic";
 $film1_description = "Il transatlantico Titanic, considerato un gioiello tecnologico ed il più lussuoso piroscafo da crociera mai realizzato...";
 $film1_year = 1997;
 
-$film1 = new Movie($film1_title, $film1_description, $film1_year);
+$film1 = new Movie($film1_title, $film1_description, $film1_year, new Genre("Melodramma", ""));
 var_dump($film1);
 
 $film2_title = "Dune";
 $film2_description = "Nell'anno 1019, la famiglia Atreides, destinata al governo del pianeta Arrakis, fondamentale per...";
 $film2_year = 1984;
 
-$film2 = new Movie($film2_title, $film2_description, $film2_year);
+$film2 = new Movie($film2_title, $film2_description, $film2_year, new Genre("Fantascienza", ""));
 var_dump($film2);
 
 
