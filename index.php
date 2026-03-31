d75f0f215cc8cb4f8bc6dc93b26466d2c71cf9b8
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-03-31 21:41:58 +0200

    feat: class Movie updated. Added atribute, metod and constructor

--- index.php
+++ index.php
@@ -2,12 +2,61 @@
 // definizione di una classe Movie
 class Movie
 {
+    protected $title;
+    protected $description;
+    protected $year;
+
+    public function __construct($_title, $_description, $_year)
+    {
+        $this->title = $_title;
+        $this->description = $_description;
+        $this->year = $_year;
+
+    }
+
+    public function setTitle($_title)
+    {
+        $this->title = $_title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setDescription($_description)
+    {
+        $this->description = $_description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function setYear($_year)
+    {
+        $this->year = $_year;
+    }
+
+    public function getYear()
+    {
+        return $this->year;
+    }
 
 }
 
-$film = new Movie();
+$film1_title = "Titanic";
+$film1_description = "Il transatlantico Titanic, considerato un gioiello tecnologico ed il più lussuoso piroscafo da crociera mai realizzato...";
+$film1_year = 1997;
 
-var_dump($film);
+$film1 = new Movie($film1_title, $film1_description, $film1_year);
+var_dump($film1);
+
+$film2_title = "Dune";
+$film2_description = "Nell'anno 1019, la famiglia Atreides, destinata al governo del pianeta Arrakis, fondamentale per...";
+$film2_year = 1984;
+
+$film2 = new Movie($film2_title, $film2_description, $film2_year);
+var_dump($film2);
 
 
 ?>
